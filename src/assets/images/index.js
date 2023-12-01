dcd3736aef1c979c045afb0731e3eb308f0a2142
IamArshadAli <arshadalikaldane@gmail.com>
2023-12-01 23:26:59 +0530

    assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import patternBackgroundDesktop from "./background-pattern-desktop.svg";
+import patternBackgroundMobile from "./background-pattern-mobile.svg";
+import minusIcon from "./icon-minus.svg";
+import plusIcon from "./icon-plus.svg";
+import starIcon from "./icon-star.svg";
+
+export {
+  patternBackgroundDesktop,
+  patternBackgroundMobile,
+  minusIcon,
+  plusIcon,
+  starIcon,
+};
