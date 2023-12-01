469a886004806f8e211a003a6949756e218b6e86
IamArshadAli <arshadalikaldane@gmail.com>
2023-12-01 23:27:28 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        WorkSans: ["Work Sans", "sans-serif"],
+      },
+      colors: {
+        White: "hsl(0, 0%, 100%)",
+        LightPink: "hsl(275, 100%, 97%)",
+        GrayishPurple: "hsl(292, 16%, 49%)",
+        DarkPurple: "hsl(292, 42%, 14%)",
+      },
+    },
+  },
+  plugins: [],
+};
