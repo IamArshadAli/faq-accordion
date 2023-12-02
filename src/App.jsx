1feb6ae85d995b26e9982cf2366cd5c1f187e8dc
IamArshadAli <arshadalikaldane@gmail.com>
2023-12-02 21:27:47 +0530

    faq accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,58 @@
-import { useMediaQuery } from "react-responsive";
-import {
-  patternBackgroundDesktop,
-  patternBackgroundMobile,
-  minusIcon,
-  plusIcon,
-  starIcon,
-} from "./assets/images";
-
 const App = () => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 768px)",
-  });
-
   return (
-    <main className="bg-LightPink w-screen h-screen font-WorkSans flex flex-col items-center px-5">
-      <figure
-        className="w-screen min-h-[14.5rem]"
-        style={{
-          backgroundImage: `url("${
-            isMobile ? patternBackgroundMobile : patternBackgroundDesktop
-          }")`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "top",
-          backgroundSize: "cover",
-        }}
-      ></figure>
-      <section className="bg-White rounded-lg px-6 py-8 -mt-[6.55rem] max-w-xs">
-        <h2 className="font-bold text-4xl">FAQs</h2>
-        <details open>
-          <summary>What is Frontend Mentor, and how will it help me?</summary>
-          <p>
+    <main className="main">
+      <figure className="main__background"></figure>
+      <section className="card">
+        <div className="card__header">
+          <figure className="header__icon"></figure>
+          <h2 className="header__title">FAQs</h2>
+        </div>
+        {/* FAQs */}
+        <details open className="faq group">
+          <summary className="faq__question">
+            What is Frontend Mentor, and how will it help me?
+          </summary>
+          <p className="faq__answer">
             Frontend Mentor offers realistic coding challenges to help
             developers improve their frontend coding skills with projects in
-            HTML, CSS, and JavaScript. It&quot;s suitable for all levels and
+            HTML, CSS, and JavaScript. It&apos;s suitable for all levels and
             ideal for portfolio building.
           </p>
         </details>
-        <details>
-          <summary>Is Frontend Mentor free?</summary>
-          <p>
+
+        <hr className="faq__divider" />
+
+        <details className="faq group">
+          <summary className="faq__question">Is Frontend Mentor free?</summary>
+          <p className="faq__answer">
             Yes, Frontend Mentor offers both free and premium coding challenges,
             with the free option providing access to a range of projects
             suitable for all skill levels.
           </p>
         </details>
-        <details>
-          <summary>Can I use Frontend Mentor projects in my portfolio?</summary>
-          <p>
+
+        <hr className="faq__divider" />
+
+        <details className="faq group">
+          <summary className="faq__question">
+            Can I use Frontend Mentor projects in my portfolio?
+          </summary>
+          <p className="faq__answer">
             Yes, you can use projects completed on Frontend Mentor in your
-            portfolio. It&quot;s an excellent way to showcase your skills to
+            portfolio. It&apos;s an excellent way to showcase your skills to
             potential employers!
           </p>
         </details>
-        <details>
-          <summary>
-            How can I get help if I&quot;m stuck on a Frontend Mentor challenge?
+
+        <hr className="faq__divider" />
+
+        <details className="faq group md:mt-7">
+          <summary className="faq__question">
+            How can I get help if I&apos;m stuck on a challenge?
           </summary>
-          <p>
-            The best place to get help is inside Frontend Mentor&quot;s Discord
-            community. There&quot;s a help channel where you can ask questions
+          <p className="faq__answer">
+            The best place to get help is inside Frontend Mentor&apos;s Discord
+            community. There&apos;s a help channel where you can ask questions
             and seek support from other community members.
           </p>
         </details>
